41ea5e603af83dc1c06def21b10702020dbf5f0d
pmcorrea <pmcorrea@me.com>
2020-05-14 04:55:42 -0400

    Minot fixes.

--- test/setup.js
+++ test/setup.js
@@ -8,7 +8,7 @@ process.env.TZ = "UTC";
 process.env.NODE_ENV = "test";
 process.env.JWT_SECRET = "secret123TEST";
 process.env.TEST_DB_URL =
-	process.env.TEST_DB_URL || "";
+	process.env.TEST_DB_URL || "postgresql://postgres@localhost/strides_test";
 
 const { expect } = require("chai");
 const supertest = require("supertest");
