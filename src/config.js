5e073ab47c1aa8a833ba9cf1c7fcc78540512cff
pmcorrea <pmcorrea@me.com>
2020-05-16 16:02:36 -0400

    Updated config for deployment.

--- src/config.js
+++ src/config.js
@@ -5,6 +5,6 @@ module.exports = {
   DATABASE_URL: process.env.DATABASE_URL || "postgresql://postgres@localhost/strides",
   TEST_DB_URL: process.env.TEST_DB_URL || "postgresql://postgres@localhost/strides_test",
   JWT_SECRET: process.env.JWT_SECRET || "secret123",
-  // CLIENT_ENDPOINT: process.env.CLIENT_ENDPOINT || "https://strides.now.sh",
-	CLIENT_ENDPOINT: "http://localhost:3000",
+  CLIENT_ENDPOINT: process.env.CLIENT_ENDPOINT || "https://strides.now.sh",
+	// CLIENT_ENDPOINT: "http://localhost:3000",
 }
