9a9b587bf778c125ca87145f021909898f80ba08
pmcorrea <pmcorrea@me.com>
2020-05-16 15:56:23 -0400

    Added tests.

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,13 +1,27 @@
 // Ensure tests are properly working
+const axios = require('axios')
 const app = require("../src/app")
 const knex = require("knex")
 const helpers = require("./test_helpers")
 const jwt = require("jsonwebtoken")
 
-describe("App", () => {
-  it("GET / responds with 200 containing 'Hello, world!'", () => {
-    return supertest(app)
-      .get("/")
-      .expect(200, "Hello, world!")
+
+describe("GraphQL Endpoint is up", () => {
+
+  it("Should login demo user and return id of '1'", async () => {
+
+        let result = await axios.post("http://localhost:5000/graphql", {
+          query: `
+          query {
+            loginUser(user_name: "Peter", user_password: "adminpassword") {
+              id
+              user_name
+              token
+            }
+          }
+          `
+        })
+
+    expect(result.data.data.loginUser.id).to.be.equal("1")
   })
 })
